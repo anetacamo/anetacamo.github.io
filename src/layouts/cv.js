23c66b1de38f74536d8d0c5bd6c962bd3ee0f19f
Aneta <anc@praqma.net>
2023-03-05 12:14:59 +0100

    updated cv

--- src/layouts/cv.js
+++ src/layouts/cv.js
@@ -30,10 +30,6 @@ const Cv = () => {
             <Href href='mailto:anetacamova@gmail.com?subject=greeting'>
               anetacamo@gmail.com
             </Href>
-            {/* &nbsp;&nbsp; | &nbsp;&nbsp;
-            <Href href='https://www.instagram.com/anetacamo'>
-              illustrations
-            </Href> */}
             <br />
             <p style={{ marginTop: 4 }}>
               born in Prague &nbsp; | &nbsp; based in Aarhus &nbsp;
@@ -41,18 +37,21 @@ const Cv = () => {
             <div className='divider'></div>
             <p>
               I have been working as a freelance or employed front-ender mostly
-              in React and Next JS with a lot of interest into designing and
-              illustrating the layouts since I got introduced to basic HTML in
-              2012.
+              for the last four years. Before getting introduced to my favourite
+              combo of React, Next and typescript I did a lot of in jekyll and
+              liquid and was tweaking my own wordpress templates. I had also a
+              chance to work with Angular, React Native, Vue. On top of that, I
+              dont usually turn down offer to design and implement my own
+              illustrations to the project.
             </p>
             <p>
               {' '}
-              I have an eye on small details, smooth transitions and all sorts
-              of details that can enhance the whole expirence of using a web or
-              an application on all devices. I really enjoy building things with
-              react, next, typescript and sass but also love to learn new things
-              and am not afraid to set up own server or hook the web to any kind
-              of database.
+              I beliebve I have eye for small details, smooth transitions and
+              enjoy making sure everything flows on all devices to implementing
+              the SEO practices. I have also learnt a lot about accessibility in
+              my previous poject. I like to automate things and make code as
+              modular and reusable as possible and am not afraid to set up own
+              server or hook the web to any kind of api or database.
             </p>
             <h2>TOOLSTACK</h2>
             {cv.cv.toolstack.map((tool) => (
@@ -94,6 +93,7 @@ const Cv = () => {
                   <h2>
                     <Href href={ref.link}>{ref.title}</Href>
                   </h2>
+                  <p style={{ marginTop: '-4px' }}>{ref.time}</p>
                   <p>{ref.text}</p>
                   <div className='tags'>
                     {ref.tags.map((tag) => (
